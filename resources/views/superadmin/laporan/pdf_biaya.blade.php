00443934300b2f3cca2d841b43c990b0521ea982
asrani <asrani.27@gmail.com>
2025-06-12 21:15:28 +0800

    f

--- resources/views/superadmin/laporan/pdf_biaya.blade.php
+++ resources/views/superadmin/laporan/pdf_biaya.blade.php
@@ -32,31 +32,112 @@
     <br />
     <table width="100%" border="1" cellpadding="5" cellspacing="0">
         <tr>
-            <th>No</th>
-            <th>Kode Produksi</th>
-            <th>Nama Produksi</th>
-            <th>Bahan</th>
-            <th>Jenis</th>
-            <th>Tenaga Kerja</th>
-            <th>Biaya</th>
-            <th>Harga Produksi</th>
-            <th>Harga Jual</th>
+            <th rowspan="2">No</th>
+            <th rowspan="2">Kode Produksi</th>
+            <th rowspan="2">Nama Produksi</th>
+            <th rowspan="2">Bahan</th>
+            <th rowspan="2">Jenis</th>
+            <th rowspan="2">Tenaga Kerja</th>
+            <th colspan="3" class="text-center">Perhitungan Full Cost</th>
+            <th rowspan="2">Aksi</th>
+        </tr>
+        <tr>
+            <th>Biaya Produksi</th>
+            <th>HPP</th>
+            <th>Laba / Rugi</th>
         </tr>
         @php
         $no =1;
         @endphp
 
         @foreach ($data as $key => $item)
-        <tr>
+        <tr style="font-size:14px">
             <td>{{$key + 1}}</td>
             <td>{{$item->kode}}</td>
             <td>{{$item->nama}}</td>
             <td>{{$item->bahan == null ? '': $item->bahan->nama}}</td>
             <td>{{$item->jenis == null ? '': $item->jenis->nama}}</td>
             <td>{{$item->tenagakerja == null ? '': $item->tenagakerja->nama}}</td>
-            <td>{{number_format($item->biaya)}}</td>
-            <td>{{number_format($item->harga_produksi)}}</td>
-            <td>{{number_format($item->harga_jual)}}</td>
+            <td>
+                <table width="100%">
+                    <tr>
+                        <td>Bahan Baku</td>
+                        <td class="text-right">{{number_format($item->bahan->harga)}}</td>
+                    </tr>
+                    <tr>
+                        <td>Tenaga Kerja</td>
+                        <td class="text-right">{{number_format($item->tenagakerja->upah->jumlah)}}</td>
+                    </tr>
+                    <tr>
+                        <td>Overhead Variabel</td>
+                        <td class="text-right">{{number_format($item->overhead->overhead_variabel)}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Overhead Tetap</td>
+                        <td class="text-right">{{number_format($item->overhead->overhead_tetap)}}</td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td class="text-right">{{number_format($item->totalBiaya())}}</td>
+                    </tr>
+                </table>
+
+            </td>
+            <td>
+                <table width="100%">
+                    <tr>
+                        <td>Persediaan barang awal jadi</td>
+                        <td class="text-right">{{number_format($item->barang_awal)}}</td>
+                    </tr>
+                    <tr>
+                        <td>HPP</td>
+                        <td class="text-right">{{number_format($item->totalBiaya())}}</td>
+                    </tr>
+                    <tr>
+                        <td>Persediaan barang akhir jadi</td>
+                        <td class="text-right">{{number_format($item->barang_akhir)}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td class="text-right">{{number_format($item->HPP())}}</td>
+                    </tr>
+                </table>
+            </td>
+            <td>
+                <table width="100%">
+                    <tr>
+                        <td>Total Penjualan </td>
+                        <td class="text-right">{{number_format($item->penjualan)}}</td>
+                    </tr>
+                    <tr>
+                        <td>HPP</td>
+                        <td class="text-right">{{number_format($item->HPP())}}</td>
+                    </tr>
+                    <tr>
+                        <td>Hasil</td>
+                        <td class="text-right">{{number_format($item->penjualan - $item->HPP())}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Keterangan</td>
+                        <td class="text-right">
+                            @if (number_format($item->penjualan - $item->HPP() > 0))
+                            Laba
+                            @else
+                            Rugi
+                            @endif
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td class="text-right">
+                <a href="/superadmin/biaya/edit/{{$item->id}}" class="btn btn-sm btn-success"><i
+                        class="fa fa-edit"></i></a>
+                <a href="/superadmin/biaya/delete/{{$item->id}}" class="btn btn-sm btn-danger"
+                    onclick="return confirm('Yakin ingin dihapus?');"><i class="fa fa-trash"></i></a>
+            </td>
         </tr>
         @endforeach
     </table>
